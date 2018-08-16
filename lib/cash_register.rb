622c670bc8630e83d26fa20202c4bcba197b0c5e
Sharon Mun <sharonymun@gmail.com>
2018-08-16 14:57:31 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
-
+require'pry'
+class CashRegister
+  attr_accessor :total
+  def initialize(total = 0)
+    @total = total
+  end
+  def discount
+    self.total 
+  end
+  def add_item(title, price, quantity = 1)
+    self.total = self.total+quantity*price
+  end
+  def apply_discount
+    discounted_percent = discount.to_f/100
+    discounted_price = discounted_percent*self.total
+    self.total - discounted_price
+  end
+end
