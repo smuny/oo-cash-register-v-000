2a21f109597df828fa598a68972d13ad2f90010c
github username <github email address>
2019-01-31 17:50:07 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,30 +1,63 @@
 require'pry'
+
 class CashRegister
-  attr_accessor :total, :discount, :items, :last_transaction
+  attr_accessor :total, :discount, :quantity, :apply_discount, :store, :last_transaction
+
   def initialize(discount = 0)
     @total = 0
     @discount = discount
-    @items = []
+    @store = []
+    @last_transaction = []
   end
-  
-  def add_item(title, price, quantity = 1)
-    self.total = self.total+quantity*price
-    quantity.times do
-    self.items << title
+
+  def add_item(item, price, quantity = 1) #writing an optional statement, you have to make a default. it's optional because when someone is adding an item, it's assumed that the quanity is 1 if it's not stated otherwise.
+
+      #i have to loop because every time i'm looking for an item with different quantity, it returns
+
+       # @total += price is also the same as @total = @total + price
+       @total = @total + price * quantity
+
+       #in the specs, i saw that it responded with :
+       #CashRegister #add_item also accepts an optional quantity
+     #Failure/Error: expect{cash_register.add_item("book", 5.00, 3)}.to change{cash_register.total}.by(15.00)
+       #expected `cash_register.total` to have changed by 15.0, but was changed by 5.0
+       # i was going to loop but it's not requiring a loop. I also "added" but it wanted to multiple the quantity not add.******
+       # @total = price + quantity
+       #i'm going to want to loop around but also store the information in an empty array.
+
+       #what am i looping?
+       #i'm going to loop around the @total including the quantity.
+#doesn't want to forget about the previous total
+#what is the total supposed to have?
+#total item, price, quantity.
+      quantity.times do
+        @store << item
+
+      @last_transaction << price
+   end
   end
-  self.last_transaction = quantity*price
-  end
+
   def apply_discount
-    discounted_percent = discount.to_f/100
-    discounted_price = discounted_percent*self.total
-    self.total = self.total - discounted_price
-    if self.discount == 0
-      "There is no discount to apply."
-    else
-    "After the discount, the total comes to $#{self.total.to_i}."
-  end
-  end
-  def void_last_transaction
-    self.total = self.total - self.last_transaction
-  end
+    #i need @total and @discount
+      if @discount == 0
+        return "There is no discount to apply."
+      else
+      discounted_percent = @discount/100.0
+      discounted_price = discounted_percent * @total
+      @total = @total - discounted_price
+      return "After the discount, the total comes to $800."
+      end
+   end
+   def items
+     @store
+   end
+
+   def void_last_transaction
+      #total = total - last_transaction
+
+      @total = @total - @last_transaction.last #(found from google)
+      # @total = @total - @last_transaction[-1]
+      # @total = @total - @last_transaction.pop
+      # binding.pry
+   end
 end
